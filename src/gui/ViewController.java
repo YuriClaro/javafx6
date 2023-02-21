8d41382d729ddd7e503c23dc81a6c3bc1bd878af
Yuri Claro <yuri.claro@outlook.com>
2023-02-21 11:00:02 -0300

    Tratando eventos com JavaFX

---
+++ src/gui/ViewController.java
@@ -0,0 +1,12 @@
+package gui;
+
+import javafx.scene.control.Button;
+
+public class ViewController {
+
+	private Button BtTest;
+	
+	public void onBtTestAlert() {
+		System.out.println("Click");
+	}
+}
